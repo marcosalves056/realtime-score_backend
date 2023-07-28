e7a65a747b6720f540ddfbee486398c78e03d55c
marcosalves056 <57968929+marcosalves056@users.noreply.github.com>
2023-07-28 00:02:11 -0300

    feat: nome do jogador na api /dados

--- index.js
+++ index.js
@@ -20,136 +20,163 @@ const connection = mysql.createPool({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/dados", (req, res) => {
+app.get("/dados", async (req, res) => {
   const jsonData = {
-    dados: [] || null,
+    dados: [],
   };
 
-  connection.query("SELECT * FROM entidades", async (err, entidades) => {
-    if (err) {
-      console.error("Erro ao buscar entidades:", err);
-      res.status(500).json({ error: "Erro ao buscar entidades" });
-      return;
-    }
+  try {
+    const entidades = await getEntidades();
 
-    const fetchTorneios = (entidade) => {
-      return new Promise((resolve, reject) => {
-        connection.query(
-          "SELECT * FROM torneios WHERE id_entidade = ?",
-          entidade.id,
-          (err, torneios) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(torneios);
-            }
-          }
-        );
-      });
-    };
+    for (const entidade of entidades) {
+      const entidadeObj = {
+        nome_entidade: entidade.nome,
+        torneios: [],
+      };
 
-    const fetchQuadras = (torneio) => {
-      return new Promise((resolve, reject) => {
-        connection.query(
-          "SELECT * FROM quadras WHERE id_torneio = ?",
-          torneio.id,
-          (err, quadras) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(quadras);
-            }
-          }
-        );
-      });
-    };
+      const torneios = await getTorneios(entidade.id);
 
-    const fetchEsporte = (torneio) => {
-      return new Promise((resolve, reject) => {
-        connection.query(
-          "SELECT nome FROM esportes WHERE id = ?",
-          torneio.id_esporte,
-          (err, esporte) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(esporte[0].nome);
-            }
-          }
-        );
-      });
-    };
+      for (const torneio of torneios) {
+        const quadras = await getQuadras(torneio.id);
 
-    const fetchEntidadesAndBuildJson = async () => {
-      for (const entidade of entidades) {
-        const entidadeObj = {
-          nome_entidade: entidade.nome,
-          torneios: [],
-        };
+        for (const quadra of quadras) {
+          const jogadores = await getJogadoresFromQuadra(quadra);
 
-        // Fetch torneios for each entidade
-        try {
-          const torneios = await fetchTorneios(entidade);
+          const quadraObj = {
+            categoria: quadra.categoria,
+            estado: quadra.estado,
+            games: [JSON.parse(quadra.games)],
+            jogador1: jogadores[0]?.nome || null,
+            jogador2: jogadores[1]?.nome || null,
+            jogador3: jogadores[2]?.nome || null,
+            jogador4: jogadores[3]?.nome || null,
+            pontos: JSON.parse(quadra.pontos),
+            quadra: quadra.quadra,
+            rodada: quadra.rodada,
+            status: quadra.status,
+            nome: quadra.nome || null, // Novo campo "nome" adicionado na tabela "quadras"
+          };
 
-          for (const torneio of torneios) {
-            const quadras = [];
-
-            // Fetch quadras for each torneio
-            const quadrasData = await fetchQuadras(torneio);
-
-            for (const quadra of quadrasData) {
-              // Fetch games for each quadra
-
-              const quadraObj = {
-                categoria: quadra.categoria,
-                estado: quadra.estado,
-                games: [JSON.parse(quadra.games)],
-                jogador1: quadra.jogador1,
-                jogador2: quadra.jogador2,
-                jogador3: quadra.jogador3,
-                jogador4: quadra.jogador4,
-                pontos: JSON.parse(quadra.pontos),
-                quadra: quadra.quadra,
-                rodada: quadra.rodada,
-                estado: quadra.status,
-                quadra: quadra.quadra,
-              };
-
-              quadras.push(quadraObj);
-            }
-
-            // Fetch esporte for each torneio
-            const esporte = await fetchEsporte(torneio);
-
-            const torneioObj = {
-              nome_torneio: torneio.nome,
-              quadras: quadras,
-            };
-
-            entidadeObj.torneios.push(torneioObj);
-          }
-
-          // Fetch esporte for each entidade
-          const esporteEntidade = await fetchEsporte(torneios[0]); // Assumindo que todos os torneios de uma entidade possuem o mesmo esporte
-
-          jsonData.dados.push({
-            entidades: [entidadeObj],
-            esporte: esporteEntidade,
-          });
-        } catch (err) {
-          console.error("Erro ao buscar torneios:", err);
+          entidadeObj.torneios.push(quadraObj);
         }
       }
 
-      res.json(jsonData);
-    };
+      const esporteEntidade = await getEsporte(entidades[0].id);
 
-    fetchEntidadesAndBuildJson().catch((err) => {
-      console.error("Erro ao buscar informações:", err);
-      res.status(500).json({ error: "Erro ao buscar informações" });
+      jsonData.dados.push({
+        entidades: [entidadeObj],
+        esporte: esporteEntidade,
+      });
+    }
+
+    res.json(jsonData);
+  } catch (err) {
+    console.error("Erro ao buscar informações:", err);
+    res.status(500).json({ error: "Erro ao buscar informações" });
+  }
+});
+
+async function getEntidades() {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM entidades", (err, entidades) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(entidades);
+      }
     });
   });
-});
+}
+
+async function getTorneios(idEntidade) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM torneios WHERE id_entidade = ?",
+      idEntidade,
+      (err, torneios) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(torneios);
+        }
+      }
+    );
+  });
+}
+
+async function getQuadras(idTorneio) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM quadras WHERE id_torneio = ?",
+      idTorneio,
+      (err, quadras) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(quadras);
+        }
+      }
+    );
+  });
+}
+
+async function getJogadoresFromQuadra(quadra) {
+  const jogadores = [];
+
+  if (quadra.jogador1) {
+    const jogador1 = await getJogador(quadra.jogador1);
+    jogadores.push(jogador1);
+  }
+
+  if (quadra.jogador2) {
+    const jogador2 = await getJogador(quadra.jogador2);
+    jogadores.push(jogador2);
+  }
+
+  if (quadra.jogador3) {
+    const jogador3 = await getJogador(quadra.jogador3);
+    jogadores.push(jogador3);
+  }
+
+  if (quadra.jogador4) {
+    const jogador4 = await getJogador(quadra.jogador4);
+    jogadores.push(jogador4);
+  }
+
+  return jogadores;
+}
+
+async function getJogador(idJogador) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM jogadores WHERE id = ?",
+      idJogador,
+      (err, jogadores) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(jogadores[0]);
+        }
+      }
+    );
+  });
+}
+
+async function getEsporte(idTorneio) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT esportes.nome FROM esportes JOIN torneios ON esportes.id = torneios.id_esporte WHERE torneios.id = ?",
+      idTorneio,
+      (err, esporte) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(esporte[0].nome);
+        }
+      }
+    );
+  });
+}
 
 // Rotas CRUD para a tabela 'jogadores'
 app.get("/jogadores", (req, res) => {
